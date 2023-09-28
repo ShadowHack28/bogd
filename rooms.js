bf55c89c843d8a25c3e78dfba27588f7b85bfb45
ShadowHack28 <119912854+ShadowHack28@users.noreply.github.com>
2023-09-27 17:36:44 -0700

    Add files via upload

---
+++ rooms.js
@@ -0,0 +1,13 @@
+function logout() {
+    window.location = "index.html";
+}
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDbd7bFnN36tF45SSpvdLi-qTA88mHdb14",
+    authDomain: "bogdog-chat-app.firebaseapp.com",
+    projectId: "bogdog-chat-app",
+    storageBucket: "bogdog-chat-app.appspot.com",
+    messagingSenderId: "620163663476",
+    appId: "1:620163663476:web:dcb595e36570cd28ad3321"
+  };
